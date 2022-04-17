4ca8c40be9089dc5462734abd143bd80f29a4370
Leonardo <leonardo.imoveis@live.com>
2022-04-17 19:18:28 -0300

    Criados atribudos de complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=36):
+    def __init__(self, *filhos, nome=None, idade=36):
         self.idade = idade
-        self.nome = None
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Leonardo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Leonardo'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    Leonardo = Pessoa(renzo, nome='Leonardo')
+    print(Pessoa.cumprimentar(Leonardo))
+    print(id(Leonardo))
+    print(Leonardo.cumprimentar())
+    print(Leonardo.nome)
+    print(Leonardo.idade)
+    for filhos in Leonardo.filhos:
+        print(filhos.nome)
+
